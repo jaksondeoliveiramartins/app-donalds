cb2fbe470bf67ee325b248ffcddcff1e7e63c6d9
jaksondeoliveiramartins <jaomfive@hotmail.com>
2025-03-16 12:20:23 -0300

    feat: add orders page

--- src/app/[slug]/menu/components/actions/create-order.ts
+++ src/app/[slug]/menu/components/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -66,4 +67,5 @@ export const createOrder = async (input: CreateOrderInput) => {
       },
     },
   });
+  redirect(`/${input.slug}/orders`);	
 };
