ebee7ac4e01d00bf33365b4d4c5f393d71039b1e
jaksondeoliveiramartins <jaomfive@hotmail.com>
2025-03-25 22:45:59 -0300

    fix:ensure orders   page cache is  properly revalidated

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -72,7 +72,7 @@ export async function POST(request: Request) {
           },
         },
       });
-      revalidatePath(`/${order.restaurant.slug}/menu`);
+      revalidatePath(`/${order.restaurant.slug}/orders`);
       break;
     }
   }
